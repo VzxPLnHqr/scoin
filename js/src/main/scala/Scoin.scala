7378a2648e4aabf2929e21eca9f972534151671d
VzxPLnHqr <>
2023-02-27 21:43:23 +0000

    fix fastLinkJS / esBuild

---
+++ js/src/main/scala/Scoin.scala
@@ -0,0 +1,11 @@
+package scoin
+
+import scala.scalajs.js.annotation._
+
+@JSExportTopLevel("Scoin")
+object Scoin {
+
+  @JSExport
+  def sayHello(): Unit = println("hello javascript land, from Scoin!")
+
+}
