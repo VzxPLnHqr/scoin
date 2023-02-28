471d5a25660636d61e97ccd074b563dcb6d5da96
VzxPLnHqr <>
2023-02-28 06:24:47 +0000

    scoin-js - Script.pay2tr wrapper

--- js/src/main/scala/Scoin.scala
+++ js/src/main/scala/Scoin.scala
@@ -16,4 +16,17 @@ object ScoinJS {
   class TransactionJS(tx: Transaction) {
     def version = tx.version.toInt
   }
+
+  @JSExportTopLevel("Script")
+  object ScriptJS {
+    /**
+      * 
+      *
+      * @param xOnlyPublicKey (as hex string)
+      * @return a scriptPubKey for taproot (as hex string)
+      */
+    @JSExport
+    def pay2tr(xOnlyPublicKey: String): String =
+      Script.write(Script.pay2tr(Crypto.XOnlyPublicKey(ByteVector32.fromValidHex(xOnlyPublicKey)))).toHex
+  }
 }
